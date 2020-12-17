f14a0b7f975d42100d9b3495f7e11c0f687fa545
unknown <espinor9@gmatc.matc.edu>
2020-12-16 19:33:28 -0600

    Completed Activity 5

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 
 const app = express();
@@ -70,10 +72,25 @@ app.post(
 
                 //save to the db and return
                 await user.save();
-                res.send('User successfully registered');
+
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr'},
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token});
+                    }
+                );
             } catch (error) {
                 res.status(500).send('Server error');
-            }
+            }             
         }
     }
 );
